546ab59c03bca8f903dd015478c80749d109e782
Jake Garcia <bballwiz4@gmail.com>
2020-03-03 23:03:06 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,25 @@
+class Artist
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_song(name, genre)
+    song = Song.new(name, self, genre)
+  end
+
+  def songs
+    Songs.all.select do |song|
+      song.artist == self
+    end
+  end
+
+end
